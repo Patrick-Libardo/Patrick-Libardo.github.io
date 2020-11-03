9e78837982620a406a6238b0137f21f647ddde61
Patrick Libardo <patricklibardoo@gmail.com>
2020-11-03 16:40:08 -0200

    Movimentar Cacto

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -17,8 +17,8 @@ function pular() {
       clearInterval(intervaloPulo);
       let intervaloQueda = setInterval(() => {
         if (dinoPosition <= 0) {
-            estaPulando = false;
-            clearInterval(intervaloQueda);
+          estaPulando = false;
+          clearInterval(intervaloQueda);
         } else {
           // Decrementar a posição do Dino
           dinoPosition -= 20;
@@ -34,3 +34,29 @@ function pular() {
     }
   }, 20);
 }
+
+// Criação de Cactos
+const background = document.querySelector(".background");
+function criarCacto() {
+  let cactoPosition = 1000;
+  const cacto = document.createElement("div");
+  // Tempo aleatório entre 1s e 6s
+  let tempoRandom = Math.random() * 5000 + 100;
+  cacto.classList.add("cacto");
+  cacto.style.left = 1000 + "px";
+  background.appendChild(cacto);
+
+  let intervaloEsquerda = setInterval(() => {
+    if (cactoPosition <= -60) {
+      clearInterval(intervaloEsquerda);
+      // Remover da Tela
+      background.removeChild(cacto);
+    } else {
+      cactoPosition -= 10;
+      cacto.style.left = cactoPosition + "px";
+    }
+  }, 20);
+  let tempoCacto = setTimeout(criarCacto, tempoRandom);
+}
+
+criarCacto();
