938628151bcc64664b5705ef6c10f796a94f87ab
Patrick Libardo <patricklibardoo@gmail.com>
2020-11-03 15:46:13 -0200

    Pulo do Dino

--- dino/js/jogo.js
+++ dino/js/jogo.js
@@ -1,1 +1,36 @@
-console.log('Início do jogo!')
+console.log("Início do jogo!");
+
+const dino = document.querySelector(".dino");
+
+let dinoPosition = 0;
+let estaPulando = false;
+
+// Verificar se a tecla espaço foi pressionada
+document.addEventListener("keydown", event => {
+  if (event.code === "Space") if (!estaPulando) pular();
+});
+
+function pular() {
+  let intervaloPulo = setInterval(() => {
+    estaPulando = true;
+    if (dinoPosition >= 250) {
+      clearInterval(intervaloPulo);
+      let intervaloQueda = setInterval(() => {
+        if (dinoPosition <= 0) {
+            estaPulando = false;
+            clearInterval(intervaloQueda);
+        } else {
+          // Decrementar a posição do Dino
+          dinoPosition -= 20;
+          // Atualizar a posição na tela
+          dino.style.bottom = dinoPosition + "px";
+        }
+      }, 20);
+    } else {
+      // Incrementar a posilção do Dino
+      dinoPosition += 20;
+      // Atualizar a posição na tela
+      dino.style.bottom = dinoPosition + "px";
+    }
+  }, 20);
+}
